f90a72039f2b509148534031302b27b20014d7fd
Keith Ingersoll <keithingersoll@gmail.com>
2016-09-04 19:28:38 -0500

    updating gpu installation for tensorflow

--- install_gpu_aws.sh
+++ install_gpu_aws.sh
@@ -3,8 +3,6 @@
 apt-get update
 apt-get -y dist-upgrade
 apt-get install -y gcc g++ gfortran build-essential git wget linux-image-generic libopenblas-dev python-setuptools python-dev python3-dev cmake libhdf5-dev xvfb libav-tools awscli
-easy_install pip
-pip install --upgrade virtualenv
 wget http://developer.download.nvidia.com/compute/cuda/repos/ubuntu1404/x86_64/cuda-repo-ubuntu1404_7.0-28_amd64.deb
 dpkg -i cuda-repo-ubuntu1404_7.0-28_amd64.deb
 apt-get update
@@ -13,5 +11,4 @@ tar -zxf cudnn-7.0-linux-x64-v4.0-prod.tgz && rm cudnn-7.0-linux-x64-v4.0-prod.t
 sudo cp -R cudnn-7.0-linux-x64-v4.0-prod/lib* /usr/local/cuda/lib64/
 sudo cp cudnn-7.0-linux-x64-v4.0-prod/cudnn.h /usr/local/cuda/include/
 echo -e "\nexport PATH=/usr/local/cuda/bin:$PATH\n\nexport LD_LIBRARY_PATH=/usr/local/cuda/lib64" >> .bashrc
-echo -e "\nsource ~/.py3/bin/activate"
 reboot
