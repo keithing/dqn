d2b0c329097f5fee3dbd546de2c1ab68cb293ee5
Keith Ingersoll <keithingersoll@gmail.com>
2016-09-04 19:31:52 -0500

    fixing record

--- record.sh
+++ record.sh
@@ -1,1 +1,2 @@
-xvfb-run -s "-screen 0 1400x900x24" python dqn/play.py -d "videos" -r -e .01 -w 'iter_0.cpkt'
+# example sh record.sh "./videos" "./checkpoints/epoch_70.ckpt"
+xvfb-run -s "-screen 0 1400x900x24" python dqn/play.py -d $1 -r -e .01 -w $2
